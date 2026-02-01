29861779c01f9f743fa79fdeb09449c34f881815
yusuf-kocabiyik <kocabiyikyusuf3@gmail.com>
2026-02-01 12:01:42 +0300

    görseler update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,9 +13,9 @@ export default function Banner(){
                 <Link to="/formPage" className="cta" data-testid="order-button">ACIKTIM!</Link> 
               </div>
               <img className="pizza" src="/images/banner-gorseller/banner.png.png" alt="pizza"/>
-              <img className="malzemeler" src="public\images\banner-gorseller\div.png" alt="malzemeler"/>
-              <img className="sag-ust" src="public\images\banner-gorseller\sağ üst.png" alt="sag-ust"/>
-              <img className="sol-alt" src="public\images\banner-gorseller\sol alt.png" alt="sol-alt"/>
+              <img className="malzemeler" src="/images/banner-gorseller/div.png" alt="malzemeler"/>
+              <img className="sag-ust" src="public/images/banner-gorseller/sağ üst.png" alt="sag-ust"/>
+              <img className="sol-alt" src="public/images/banner-gorseller/sol alt.png" alt="sol-alt"/>
               
         </section>
         
